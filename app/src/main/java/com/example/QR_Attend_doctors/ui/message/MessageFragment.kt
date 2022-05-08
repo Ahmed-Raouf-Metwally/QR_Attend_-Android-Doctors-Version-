f1122d788c4327c6e1a060011731bce312cb16d3
omda <ahmed18-4>
2022-05-08 09:09:17 +0200

    attende list design adapter done working on showing some students in the list for now list of attendance is done delet student still working on it

--- app/src/main/java/com/example/QR_Attend_doctors/ui/message/MessageFragment.kt
+++ app/src/main/java/com/example/QR_Attend_doctors/ui/message/MessageFragment.kt
@@ -7,7 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.example.QR_Attend_doctors.R
 import com.example.QR_Attend_doctors.databinding.FragmentSlideshowBinding
+import com.example.QR_Attend_doctors.ui.Adapters.StudentData
+import com.example.QR_Attend_doctors.ui.Adapters.StudentsInAttendanceListAdapter
+import com.example.QR_Attend_doctors.ui.Adapters.SubjectsData
 
 class MessageFragment : Fragment() {
     private var _binding: FragmentSlideshowBinding? = null
@@ -30,7 +35,23 @@ class MessageFragment : Fragment() {
 
         return root
     }
-
+lateinit var studentsListRec : RecyclerView
+lateinit var studentsAdap : StudentsInAttendanceListAdapter
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        studentsListRec = view.findViewById(R.id.students_attendance_rec)
+        creatLsit()
+        studentsAdap = StudentsInAttendanceListAdapter(studentList)
+        studentsListRec.adapter = studentsAdap
+    }
+    lateinit var  studentList : MutableList<StudentData>
+    fun creatLsit(){
+        studentList = mutableListOf()
+        studentList.add(StudentData(studentName = "Ahmed", studentId = "119000202", studentImage = R.drawable.profile_logo))
+        studentList.add(StudentData(studentName = "mohamed", studentId = "119000203", studentImage = R.drawable.profile_logo))
+        studentList.add(StudentData(studentName = "Ali", studentId = "119000204", studentImage = R.drawable.profile_logo))
+        studentList.add(StudentData(studentName = "Mohsen", studentId = "119000205", studentImage = R.drawable.profile_logo))
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
